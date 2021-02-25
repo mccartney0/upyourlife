92314f73026800ee7e06aa9e2525f4181ddef4bb
ALLSANTO <ALLSANTO@ALLSANTO-7410.br.oracle.com>
2021-02-24 23:40:23 -0300

    Criado components Profile, Complete challenges e Countdown

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,14 +1,13 @@
-// CSS module
 import styles from '../styles/components/ExperienceBar.module.css';
 
-export function ExperienceBar(){
-    return(
-        <header className={styles.ExperienceBar}>
+export function ExperienceBar() {
+    return (
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{width: '50%'}}/>
-                <span className={styles.currentExperiencer} style={{left: '50%'}}>
-                    300xp
+                <div style={{ width: '50%' }} />
+                <span className={styles.currentExperience} style={{ left: '50%' }}>
+                    300px
                 </span>
             </div>
             <span>600 xp</span>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
-import Head from "next/head";
+import { CompleteChallenges } from "../components/CompleteChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-        rel="stylesheet"
-      />
-
+    <div className={styles.container}>
       <ExperienceBar />
+
+      {/* dividir em dois */}
+      <section>
+        <div>
+          <Profile />
+          <CompleteChallenges />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
     </div>
     // <div>
     //   <Button color="hotpink">
